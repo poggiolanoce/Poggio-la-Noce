3e1bd4935b4dc072deb19a4c4e4f5b345e4d139a
Andy Skogrand <andy.skogrand@gmail.com>
2015-01-12 11:47:01 -0800

    Basic accordion

--- public/coffeescript/main.coffee
+++ public/coffeescript/main.coffee
@@ -1,3 +1,18 @@
 $(document).ready ->
+  $page_blocks = false
+  $headlines = $('.page-text-container h2')
 
-  
+
+  # Make each section active when clicked
+  $('.page-text-container h2').on 'click', ()->
+    $page_blocks.removeClass('active')
+    $(this).next().addClass('active')
+    $(this).addClass('active')
+
+  # Wrap content for collapsing content areas
+  $('.page-text-container').each ()->
+    $headlines.each ()->
+      $(this).nextUntil('h2').wrapAll('<div class="page-text-block" />')
+    $page_blocks = $('.page-text-block')
+
+    $headlines.eq(0).trigger('click')
--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,4 +1,20 @@
 (function() {
-  $(document).ready(function() {});
+  $(document).ready(function() {
+    var $headlines, $page_blocks;
+    $page_blocks = false;
+    $headlines = $('.page-text-container h2');
+    $('.page-text-container h2').on('click', function() {
+      $page_blocks.removeClass('active');
+      $(this).next().addClass('active');
+      return $(this).addClass('active');
+    });
+    return $('.page-text-container').each(function() {
+      $headlines.each(function() {
+        return $(this).nextUntil('h2').wrapAll('<div class="page-text-block" />');
+      });
+      $page_blocks = $('.page-text-block');
+      return $headlines.eq(0).trigger('click');
+    });
+  });
 
 }).call(this);
