0cf60d3c9d40138a83da204abba2fe5874a2390d
Andy Skogrand <andy.skogrand@gmail.com>
2015-01-13 12:03:16 -0800

    Basics for slideshow

--- public/coffeescript/main.coffee
+++ public/coffeescript/main.coffee
@@ -5,6 +5,8 @@ $(document).ready ->
   $wine_selection = $('.wine-browser li')
   $wine_content =  $('.wine-page-content')
 
+  is_transitioning = false
+
   # Make each section active when clicked
   $('.page-text-container h2').on 'click', ()->
     if $(this).hasClass('active')
@@ -32,3 +34,30 @@ $(document).ready ->
     $wine_content.removeClass('active')
     $selection = $("#wine-#{$(this).data('wine')}")
     $selection.addClass('active')
+
+
+  # Slider
+  if $('.banners .banner').length > 1
+    $('.banners').addClass('banner_slideshow')
+
+  $('.banner_slideshow .banner-dots li').on 'click', ()->
+    next_slide = $(this).data('slide')
+    changeSlide(next_slide)
+
+  changeSlide = (slide)->
+    $previously_active = $('.banners .banner.active')
+    $active = $('.banners .banner').eq(slide)
+
+    if $previously_active == $active then return false
+    if is_transitioning then return false
+
+    is_transitioning = true
+
+    $previously_active.removeClass('active').addClass('previously_active')
+    $active.addClass('active')
+
+    setTimeout finishChangeslide, 2000
+
+  finishChangeslide = ()->
+    $('.previously_active').removeClass('previously_active')
+    is_transitioning = false
--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,10 +1,11 @@
 (function() {
   $(document).ready(function() {
-    var $headlines, $page_blocks, $wine_content, $wine_selection;
+    var $headlines, $page_blocks, $wine_content, $wine_selection, changeSlide, finishChangeslide, is_transitioning;
     $page_blocks = false;
     $headlines = $('.page-text-container h2');
     $wine_selection = $('.wine-browser li');
     $wine_content = $('.wine-page-content');
+    is_transitioning = false;
     $('.page-text-container h2').on('click', function() {
       if ($(this).hasClass('active')) {
         $(this).next().removeClass('active');
@@ -23,7 +24,7 @@
       $page_blocks = $('.page-text-block');
       return $headlines.eq(0).trigger('click');
     });
-    return $wine_selection.on('click', function() {
+    $wine_selection.on('click', function() {
       var $selection;
       $wine_selection.removeClass('active');
       $(this).addClass('active');
@@ -31,6 +32,33 @@
       $selection = $("#wine-" + ($(this).data('wine')));
       return $selection.addClass('active');
     });
+    if ($('.banners .banner').length > 1) {
+      $('.banners').addClass('banner_slideshow');
+    }
+    $('.banner_slideshow .banner-dots li').on('click', function() {
+      var next_slide;
+      next_slide = $(this).data('slide');
+      return changeSlide(next_slide);
+    });
+    changeSlide = function(slide) {
+      var $active, $previously_active;
+      $previously_active = $('.banners .banner.active');
+      $active = $('.banners .banner').eq(slide);
+      if ($previously_active === $active) {
+        return false;
+      }
+      if (is_transitioning) {
+        return false;
+      }
+      is_transitioning = true;
+      $previously_active.removeClass('active').addClass('previously_active');
+      $active.addClass('active');
+      return setTimeout(finishChangeslide, 2000);
+    };
+    return finishChangeslide = function() {
+      $('.previously_active').removeClass('previously_active');
+      return is_transitioning = false;
+    };
   });
 
 }).call(this);
