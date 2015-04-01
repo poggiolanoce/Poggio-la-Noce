038be8eb45678bb13eda61026ebfdb14a7d62ddc
Andy Skogrand <andy.skogrand@gmail.com>
2015-04-01 13:59:35 -0700

    Translating contact page and wine types

---
+++ craft/translations/en.php
@@ -0,0 +1,19 @@
+<?php
+
+return array(
+    "Name" => "Your Name",
+    "Email" => "Your Email",
+    "Message" => "Message",
+    "Thanks" => "Thank you for your message!",
+    "Thanks2" => "Someone will be in contact with you shortly.",
+    "PreferEmail" => "Prefer email? You can reach us at",
+    "Description" => "Description",
+    "Specifications" => "Specifications",
+    "WineType" => "Wine Type",
+    "VineyardLocation" => "Vineyard Location",
+    "Grapes" => "Grapes",
+    "Exposition" => "Exposition",
+    "Soil" => "Soil",
+    "Vinification" => "Vinification and maturation",
+    "Additional" => "Additional Details"
+);
---
+++ craft/translations/it.php
@@ -0,0 +1,19 @@
+<?php
+
+return array(
+    "Name" => "Nome",
+    "Email" => "Mail",
+    "Message" => "Messaggio",
+    "Thanks" => "Grazie per il messaggio!",
+    "Thanks2" => "Vi contatteremo appena possibile.",
+    "PreferEmail" => "Preferisci email? ci puoi contattare a ",
+    "Description" => "Descrizione",
+    "Specifications" => "Specifiche",
+    "WineType" => "Tipo di Vino",
+    "VineyardLocation" => "Luogo del vigneto",
+    "Grapes" => "Uvaggio",
+    "Exposition" => "Esposizione",
+    "Soil" => "Terreno",
+    "Vinification" => "Vinification and maturation",
+    "Additional" => "Ulteriori dettagli"
+);
