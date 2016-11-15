d3caa99e17b709917c086439f57ee6885a69aa7e
Andy Skogrand <andy@bletchley.co>
2016-11-15 12:43:50 -0800

    Enable geolocation for setting currency

---
+++ craft/config/geo.php
@@ -0,0 +1,16 @@
+<?php
+
+// Place this file in your craft/config folder, nest to general.php
+
+return array(
+  // This is the IP address used if in devMode or visiting your site
+  // from the local computer to the server.
+  // US IP
+  "defaultIp"=>"72.229.28.185",
+  // EU IP
+  // "defaultIp"=>"93.51.247.104",
+
+  // This is the number of seconds to cache data about an IP address, default is 12
+  // hours if not set.
+  //"cacheTime"=>"43200"
+);
